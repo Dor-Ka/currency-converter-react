2a0f8db6ae7307ccd415a3608d9918627b9a3747
Dorota Karpińska <dorota.k159@wp.pl>
2024-02-08 19:24:46 +0100

    renamed CSS classes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
       <Form />
       <Result />
     </Container>
-
   );
 };
 
--- src/Result/index.js
+++ src/Result/index.js
@@ -2,9 +2,9 @@ import "./style.css";
 
 const Result = () => (
     <>
-        <p className="text--normal">Otrzymasz:</p>
-        <p className="text__result"> <strong> </strong> PLN</p>
-        <p className="text--normal">(na dzień: 2023-08-17)</p>
+        <p className="text">Otrzymasz:</p>
+        <p className="text text--result"> <strong> </strong> PLN</p>
+        <p className="text">(na dzień: 2023-08-17)</p>
     </>
 )
 
