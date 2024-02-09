f994611787500971b70d57a18bb8b37bc41572a8
Dorota Karpińska <dorota.k159@wp.pl>
2024-02-09 13:09:10 +0100

    added currency table file

---
+++ src/currencies.js
@@ -0,0 +1,18 @@
+const currencies = [
+    {
+;       name: "USD",
+        rate: 4,0096,
+    },
+    {
+        name: "EUR",
+        rate: 4,3167,
+    },
+    {
+        name: "CHF",
+        rate: 4,5779,
+    },
+    {
+        name: "GBP",
+        rate: 5,0559,
+    },
+];
