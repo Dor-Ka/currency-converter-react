70b18ba0030dfb0b42b91e4b164f55b148d820ce
Dorota Karpińska <dorota.k159@wp.pl>
2024-03-02 14:26:50 +0100

    change in the structure of the application

--- src/App/theme.js
+++ src/App/theme.js
@@ -4,6 +4,7 @@ export const theme = {
         ghostWhite: "#f6f6f7",
         gray: "#ccc",
         dimGray: "#666666",
-        codGray: "#0f0f0f"
+        codGray: "#0f0f0f",
+        lipstick: "#9b1003",
     },
 };
